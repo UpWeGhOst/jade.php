1d8e3f889d03e1a2aba6293621fcdfea042113a2
Alexander <upweghost@gmail.com>
2015-05-23 23:49:38 +0300

    create default method

--- src/UpWeGhOst/JadeExtender/JadeExtended.php
+++ src/UpWeGhOst/JadeExtender/JadeExtended.php
@@ -5,13 +5,33 @@
 namespace UpWeGhOst\JadeExtender;
 
 use Everzet\Jade\Dumper\DumperInterface;
+use Everzet\Jade\Filter\CDATAFilter;
+use Everzet\Jade\Filter\CSSFilter;
+use Everzet\Jade\Filter\JavaScriptFilter;
+use Everzet\Jade\Filter\PHPFilter;
 use Everzet\Jade\Jade;
+use Everzet\Jade\Lexer\Lexer;
 use Everzet\Jade\Parser;
+use Everzet\Jade\Visitor\AutotagsVisitor;
+use UpWeGhOst\JadeExtender\Dumper\PHPDumperExtended;
 
 class JadeExtended extends Jade {
 
-	public static function create(Parser $parser, DumperInterface $dumper, $cache = null) {
+	public static function create(Parser $parser = null, DumperInterface $dumper = null, $cache = null) {
+
+		if ($parser === null || $dumper === null)
+			return new self(
+				new Parser(new Lexer()),
+				PHPDumperExtended::create()
+					->registerVisitor('tag', new AutotagsVisitor())
+					->registerFilter('javascript', new JavaScriptFilter())
+					->registerFilter('cdata', new CDATAFilter())
+					->registerFilter('php', new PHPFilter())
+					->registerFilter('style', new CSSFilter())
+			);
+
 		return new self($parser, $dumper, $cache);
+
 	}
 
 	public function render($template, array $data) {
